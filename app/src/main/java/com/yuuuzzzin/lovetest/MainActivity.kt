ccbb3dad3cd5a06a3273e42d270312cedff4a83a
yuuuzzzin <uiuiui118437@gmail.com>
2021-07-01 17:07:03 +0900

    MainActivity 수정

--- app/src/main/java/com/yuuuzzzin/lovetest/MainActivity.kt
+++ app/src/main/java/com/yuuuzzzin/lovetest/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.yuuuzzzin.lovetest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
