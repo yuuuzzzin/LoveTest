29f770e1434c7bcdc4fc08230b6d0fa124353219
yuuuzzzin <uiuiui118437@gmail.com>
2021-07-01 17:08:17 +0900

    viewbinding하여 버튼 클릭 이벤트 추가

--- app/src/main/java/com/yuuuzzzin/lovetest/fragment/SelectionFragment.kt
+++ app/src/main/java/com/yuuuzzzin/lovetest/fragment/SelectionFragment.kt
@@ -1,60 +1,71 @@
 package com.yuuuzzzin.lovetest.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.yuuuzzzin.lovetest.R
+import com.yuuuzzzin.lovetest.databinding.FragmentSelectionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SelectionFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SelectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SelectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var navController : NavController
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // 뷰가 사라질 때, 즉 메모리에서 날아갈 때 같이 날리기 위해 따로 해두기
+    private var _binding: FragmentSelectionBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selection, container, false)
+        _binding = FragmentSelectionBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.btnBack.setOnClickListener(this)
+        binding.option1.setOnClickListener(this)
+        binding.option2.setOnClickListener(this)
+        binding.option3.setOnClickListener(this)
+        binding.option4.setOnClickListener(this)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.btnBack.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.option_1 -> {navigateWithIndex(1)}
+            R.id.option_2 -> {navigateWithIndex(2)}
+            R.id.option_3 -> {navigateWithIndex(3)}
+            R.id.option_4 -> {navigateWithIndex(4)}
+            R.id.btn_back -> {
+                navController.popBackStack()
             }
+        }
+    }
+
+    fun navigateWithIndex(index : Int) {
+
+        val bundle = bundleOf("index" to index)
+
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment2, bundle)
     }
 }
